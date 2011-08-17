cbd41118db54852f39292dcf6206d46fe1fda626
ianwookim <ianwookim@gmail.com>
2011-08-17 21:33:44 +0000

    using HEP.Util.GHC.Plugins

--- lib/HEP/Automation/JobQueue/Tester/Job.hs
+++ lib/HEP/Automation/JobQueue/Tester/Job.hs
@@ -1,11 +1,18 @@
 module HEP.Automation.JobQueue.Tester.Job where
 
 import HEP.Automation.JobQueue.JobQueue
-import HEP.Automation.JobQueue.Tester.Plugins
+import HEP.Automation.JobQueue.JobType
+
+import HEP.Storage.WebDAV
+
+import HEP.Util.GHC.Plugins
+import Unsafe.Coerce
 
 jobTest :: String-> String -> String -> IO ()
 jobTest datasetdir mname job = do 
-  (eventsets,webdavdir) <- pluginCompile datasetdir mname 
+  let fullmname = "HEP.Automation.MadGraph.Dataset." ++ mname
+  value <- pluginCompile datasetdir fullmname "(eventsets,webdavdir)" 
+  let (eventsets,webdavdir) = unsafeCoerce value :: ([EventSet],WebDAVRemoteDir)
   jobdetails <- case job of
                   "atlas_lhco"  -> return $ map (flip (MathAnal "atlas_lhco") webdavdir) eventsets
                   "tev_reco"    -> return $ map (flip (MathAnal "tev_reco") webdavdir) eventsets
