26caf23c56caa72f09e71c58d8e44b35e8f2c135
ianwookim <ianwookim@gmail.com>
2011-10-25 02:06:48 +0000

    log support. version bump to 0.1

--- lib/HEP/Automation/JobQueue/Tester/Command.hs
+++ lib/HEP/Automation/JobQueue/Tester/Command.hs
@@ -7,10 +7,28 @@ import HEP.Automation.JobQueue.Tester.Job
 import HEP.Automation.Pipeline.Config
 import HEP.Automation.JobQueue.Config 
 
+import qualified HEP.Automation.MadGraph.Log as MadGraphLog 
+
+import System.Log.Logger
+import System.Log.Handler.Syslog
+import System.Log.Handler.Simple
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter
+
+import System.IO
 
 commandLineProcess :: JobTester -> IO () 
 commandLineProcess (Test conf tconf mname job) = do 
   putStrLn "test called"
+  startLog MadGraphLog.defaultLogChan
   lc <- readConfigFile conf 
   tc <- readTestConfigFile tconf
   startJob lc tc mname job 
+
+startLog :: String -> IO () 
+startLog logchanname = do 
+  updateGlobalLogger logchanname (setLevel DEBUG)
+  h <- streamHandler stderr DEBUG >>= \lh -> return $
+         setFormatter lh 
+           (simpleLogFormatter "[$time : $loggername : $prio] $msg")
+  updateGlobalLogger logchanname (addHandler h) 
